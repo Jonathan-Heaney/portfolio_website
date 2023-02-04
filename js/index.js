4e86e3be5d0f630848a35416bca5b77427db0719
Jonathan Heaney <jonathan.heaney@gmail.com>
2023-02-04 17:24:48 -0600

    Start adding JS for portfolio toggle

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
+const portfolioImages = document.querySelectorAll('.portfolio__item');
 
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
@@ -10,3 +11,5 @@ navLinks.forEach((link) => {
     document.body.classList.remove('nav-open');
   });
 });
+
+portfolioImages.addEventListener('mouseover', () => {});
