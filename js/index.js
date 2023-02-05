cb7e87a9da0f5edf7eb3450aeeb3804662f72141
Jonathan Heaney <jonathan.heaney@gmail.com>
2023-02-05 10:01:25 -0600

    Fix overlay hover issue

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
-const idealCustomer = document.querySelector('#ideal-customer-img');
 const idealCustomerCover = document.querySelector('#ideal-customer-cover');
-const costCenter = document.querySelector('#cost-center-img');
+const idealCustomerOverlay = document.querySelector('#ideal-customer-overlay');
+const workforceSurveyOverlay = document.querySelector(
+  '#workforce-survey-overlay'
+);
+const workforceSurveyCover = document.querySelector('#workforce-survey-cover');
+const costCenterOverlay = document.querySelector('#cost-center-overlay');
 const costCenterCover = document.querySelector('#cost-center-cover');
 
 navToggle.addEventListener('click', () => {
@@ -15,51 +19,26 @@ navLinks.forEach((link) => {
   });
 });
 
-idealCustomer.addEventListener('mouseover', () => {
+idealCustomerOverlay.addEventListener('mouseover', () => {
   idealCustomerCover.style.opacity = '0';
 });
 
-idealCustomer.addEventListener('mouseleave', () => {
+idealCustomerOverlay.addEventListener('mouseleave', () => {
   idealCustomerCover.style.opacity = '1';
 });
 
-costCenter.addEventListener('mouseover', () => {
+workforceSurveyOverlay.addEventListener('mouseover', () => {
+  workforceSurveyCover.style.opacity = '0';
+});
+
+workforceSurveyOverlay.addEventListener('mouseleave', () => {
+  workforceSurveyCover.style.opacity = '1';
+});
+
+costCenterOverlay.addEventListener('mouseover', () => {
   costCenterCover.style.opacity = '0';
 });
 
-costCenter.addEventListener('mouseleave', () => {
+costCenterOverlay.addEventListener('mouseleave', () => {
   costCenterCover.style.opacity = '1';
 });
-
-// portfolioImages.forEach((image) => {
-//   image.addEventListener('mouseover', () => {
-//     console.log('working');
-//     hideCover;
-//     showOverlay;
-//   });
-// });
-
-// portfolioImages.forEach((image) => {
-//   image.addEventListener('mouseleave', () => {
-//     console.log('leaving');
-//     showCover;
-//     hideOverlay;
-//   });
-// });
-
-// const hideCover = portfolioCover.forEach((cover) => {
-//   cover.classList.add('cover__hidden');
-//   console.log('hideCover working');
-// });
-
-// const showOverlay = portfolioCover.forEach((overlay) => {
-//   overlay.classList.remove('overlay__hidden');
-// });
-
-// const showCover = portfolioCover.forEach((cover) => {
-//   cover.classList.remove('cover__hidden');
-// });
-
-// const hideOverlay = portfolioCover.forEach((overlay) => {
-//   overlay.classList.add('overlay__hidden');
-// });
